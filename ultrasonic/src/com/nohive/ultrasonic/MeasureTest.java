40f820029499238d94f1c4373aee5406def016ec
bju <bju@HiveHost>
2023-03-18 21:15:57 +0100

    Testing ultrasonic measurements

---
+++ ultrasonic/src/com/nohive/ultrasonic/MeasureTest.java
@@ -0,0 +1,10 @@
+package com.nohive.ultrasonic;
+
+public class MeasureTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start Measurement");
+	}
+
+}
